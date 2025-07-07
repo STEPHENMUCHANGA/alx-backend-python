2f26f7a8342b4c9d979bd13d864e3736bbbdbd76
STEPHENMUCHANGA <149421335+STEPHENMUCHANGA@users.noreply.github.com>
2025-07-07 14:47:05 +0300

    Update 2-transactional.py

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -1,1 +1,42 @@
+## Transaction Management Decorator
+### Objective
+### create a decorator that manages database transactions by automatically committing or rolling back changes
+-- a decorator transactional(func) that ensures a function running a database operation is wrapped inside a transaction.If the function raises an error, rollback; otherwise commit the transaction--
+-- with_db_connection --
+import sqlite3
+import functools
+
+## Decorator to manage DB connection
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            return func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+    return wrapper
+
+## Decorator to manage transactions
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            print(f"[ERROR] Transaction failed: {e}")
+            raise
+    return wrapper
+
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+## Update user's email with automatic connection & transaction handling
+update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
 
